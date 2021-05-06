53423ae46dcbaec6d051d217103a99212eb36b34
Yazeed AlKhalaf <yazeedfady@gmail.com>
2021-05-07 00:12:11 +0300

    ✒ Rename home_provider to main_providers

--- lib/ui/views/main_view.dart
+++ lib/ui/views/main_view.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_desktop_fireside_chat_example/app/providers/home_provider.dart';
+import 'package:flutter_desktop_fireside_chat_example/app/providers/main_providers.dart';
 
 import 'package:flutter_desktop_fireside_chat_example/ui/widgets/responsive.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';

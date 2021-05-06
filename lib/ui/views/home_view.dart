c43fd7ad7228a9b8502855f88b5e443268c6c09d
Yazeed AlKhalaf <yazeedfady@gmail.com>
2021-05-06 22:43:12 +0300

    ➕ Add responsive drawer

--- lib/ui/views/home_view.dart
+++ lib/ui/views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_desktop_fireside_chat_example/ui/widgets/responsive.dart';
 
 class HomeView extends StatefulWidget {
   @override
@@ -6,43 +7,100 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-  int _counter = 0;
-  void _incrementCounter() {
-    _counter++;
-    setState(() {});
+  @override
+  Widget build(BuildContext context) {
+    return Responsive(
+      mobile: _HomeViewMobile(),
+      desktop: _HomeViewDesktop(),
+    );
   }
+}
 
+class _HomeViewDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    _buildDrawerListTile({
+      required String title,
+      required IconData icon,
+      required void Function() onTap,
+    }) {
+      return ListTile(
+        leading: Icon(
+          icon,
+        ),
+        title: Text(
+          title,
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        onTap: onTap,
+      );
+    }
+
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          _buildDrawerListTile(
+            title: "Home",
+            icon: Icons.home_rounded,
+            onTap: () {},
+          ),
+          _buildDrawerListTile(
+            title: "Orders",
+            icon: Icons.receipt_long_rounded,
+            onTap: () {},
+          ),
+          _buildDrawerListTile(
+            title: "Customers",
+            icon: Icons.person_rounded,
+            onTap: () {},
+          ),
+          _buildDrawerListTile(
+            title: "Products",
+            icon: Icons.local_offer_rounded,
+            onTap: () {},
+          ),
+          _buildDrawerListTile(
+            title: "Analytics",
+            icon: Icons.analytics_rounded,
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _HomeViewMobile extends StatefulWidget {
+  @override
+  __HomeViewMobileState createState() => __HomeViewMobileState();
+}
+
+class __HomeViewMobileState extends State<_HomeViewMobile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(""),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "Press the 'plus' button to increase the counter:",
-            ),
-            Text(
-              "$_counter",
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _incrementCounter();
-        },
-        tooltip: "Increment",
-        child: Icon(
-          Icons.add,
-        ),
+      appBar: AppBar(),
+      drawer: _HomeViewDrawer(),
+    );
+  }
+}
+
+class _HomeViewDesktop extends StatefulWidget {
+  @override
+  __HomeViewDesktopState createState() => __HomeViewDesktopState();
+}
+
+class __HomeViewDesktopState extends State<_HomeViewDesktop> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: <Widget>[
+          _HomeViewDrawer(),
+        ],
       ),
     );
   }
